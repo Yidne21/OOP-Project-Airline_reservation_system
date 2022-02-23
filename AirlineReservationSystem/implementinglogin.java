c77ff34c65dfa200ceb696f9071fb57821d114d9
TolasaGanati <tolog766@gmail.com>
2022-02-23 22:42:18 +0300

    I update login of admin

---
+++ AirlineReservationSystem/implementinglogin.java
@@ -0,0 +1,9 @@
+Override
+boolean Login(int phonenumber, String passwored) {
+    System.out.println("enter phoneNumber: ");
+    phonenumber= input.nextInt();
+    System.out.println("enter password: ");
+    passwored= input.next();
+    // TODO Auto-generated method stub
+    return true;
+}

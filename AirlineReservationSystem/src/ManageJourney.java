e73ade57565f09fb5990971be37d3dceb3adc052
Zelalem Daniel <zelozdigi7@gmail.com>
2022-02-28 23:34:04 +0300

    I started commiting

--- AirlineReservationSystem/src/ManageJourney.java
+++ AirlineReservationSystem/src/ManageJourney.java
@@ -25,14 +25,17 @@ public class ManageJourney {
     };
 
     String Addjourney(int journeyId, String from, String destination, String rout, float cost) {
+        //start typing your code
         return "journey Added succssesfuly";
     }
 
     String updatejourney(int journeyId) {
+
         return "journey deleted succssesfuly";
     }
 
     String Deleteupdatejourney(int journeyId) {
+        
         return "journey deleted succssesfuly";
 
     }
